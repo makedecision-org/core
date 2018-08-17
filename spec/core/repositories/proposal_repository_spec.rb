75611465a63366082d17b970db068bb381091b58
Anton Davydov <antondavydov.o@gmail.com>
2018-08-17 21:03:18 +0300

    Add specs for finding proposal with variants logic

--- spec/core/repositories/proposal_repository_spec.rb
+++ spec/core/repositories/proposal_repository_spec.rb
@@ -1,3 +1,27 @@
 RSpec.describe ProposalRepository, type: :repository do
-  # place your tests here
+  let(:repo) { described_class.new }
+
+  describe '#find_with_variants' do
+    let(:proposal) { repo.create(title: 'test', body: 'test case for new proposal') }
+
+    subject { repo.find_with_variants(proposal.id) }
+
+    context 'when proposal has variants' do
+      before { VariantRepository.new.create(name: 'variant #1', proposal_id: proposal.id) }
+
+      it { expect(subject).to be_a(Proposal) }
+      it { expect(subject.variants).to all(be_a(Variant)) }
+    end
+
+    context 'when proposal does not have variants' do
+      it { expect(subject).to be_a(Proposal) }
+      it { expect(subject.variants).to eq([]) }
+    end
+
+    context 'when proposal not exist in db' do
+      subject { repo.find_with_variants(0) }
+
+      it { expect(subject).to eq nil }
+    end
+  end
 end
