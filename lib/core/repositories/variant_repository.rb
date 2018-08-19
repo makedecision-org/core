97208c7d9cfb881c9bbdbceee48369ef58910984
Anton Davydov <antondavydov.o@gmail.com>
2018-08-19 17:19:27 +0300

    Add repository methods for adding traits to specific variant

--- lib/core/repositories/variant_repository.rb
+++ lib/core/repositories/variant_repository.rb
@@ -2,4 +2,14 @@ class VariantRepository < Hanami::Repository
   associations do
     belongs_to :proposal
   end
+
+  def add_positive_trait(id, trait)
+    variant = root.by_pk(id).one!
+    update(id, positive_traits: variant.positive_traits + [{ value: trait }])
+  end
+
+  def add_negative_trait(id, trait)
+    variant = root.by_pk(id).one!
+    update(id, negative_traits: variant.negative_traits + [{ value: trait }])
+  end
 end
--- spec/core/repositories/variant_repository_spec.rb
+++ spec/core/repositories/variant_repository_spec.rb
@@ -1,3 +1,60 @@
 RSpec.describe VariantRepository, type: :repository do
-  # place your tests here
+  let(:repo) { described_class.new }
+  let(:proposal) { ProposalRepository.new.create(title: 'test', body: 'test') }
+
+  describe '#add_positive_trait' do
+    subject { repo.add_positive_trait(variant.id, 'new positive trait') }
+
+    context 'when variant contain positive traits' do
+      let(:variant) { repo.create(name: 'test', proposal_id: proposal.id, positive_traits: [{ value: 'new one' }] ) }
+
+      it 'adds one more trait to the variant' do
+        subject
+        expect(repo.find(variant.id).positive_traits).to eq([{ value: 'new one' }, { value: 'new positive trait' }])
+      end
+    end
+
+    context 'when variant have no one positive traits' do
+      let(:variant) { repo.create(name: 'test', proposal_id: proposal.id) }
+
+      it 'adds one more trait to the variant' do
+        subject
+        expect(repo.find(variant.id).positive_traits).to eq([{ value: 'new positive trait' }])
+      end
+    end
+
+    context 'when variant is not exist in db' do
+      subject { repo.add_positive_trait(0, 'new positive trait') }
+
+      it { expect { subject }.to raise_error }
+    end
+  end
+
+  describe '#add_negative_trait' do
+    subject { repo.add_negative_trait(variant.id, 'new positive trait') }
+
+    context 'when variant contain positive traits' do
+      let(:variant) { repo.create(name: 'test', proposal_id: proposal.id, negative_traits: [{ value: 'new one' }] ) }
+
+      it 'adds one more trait to the variant' do
+        subject
+        expect(repo.find(variant.id).negative_traits).to eq([{ value: 'new one' }, { value: 'new positive trait' }])
+      end
+    end
+
+    context 'when variant have no one positive traits' do
+      let(:variant) { repo.create(name: 'test', proposal_id: proposal.id) }
+
+      it 'adds one more trait to the variant' do
+        subject
+        expect(repo.find(variant.id).negative_traits).to eq([{ value: 'new positive trait' }])
+      end
+    end
+
+    context 'when variant is not exist in db' do
+      subject { repo.add_negative_trait(0, 'new positive trait') }
+
+      it { expect { subject }.to raise_error }
+    end
+  end
 end
