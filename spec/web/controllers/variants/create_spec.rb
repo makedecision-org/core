ae10a65dfbcf5e2a7838fab789b1a8bf92f38a7a
Anton Davydov <antondavydov.o@gmail.com>
2018-08-20 19:54:11 +0300

    Update specs for create variant endpoint

--- spec/web/controllers/variants/create_spec.rb
+++ spec/web/controllers/variants/create_spec.rb
@@ -4,11 +4,18 @@ RSpec.describe Web::Controllers::Variants::Create, type: :action do
 
   subject { action.call(params) }
 
-  let(:operation) { -> (proposal_id:, name:) { Success(Variant.new) } }
+  context 'when operation returns success result' do
+    let(:operation) { -> (proposal_id:, name:) { Success(Variant.new) } }
 
-  it { expect(subject).to redirect_to('/proposals/1') }
+    it { expect(subject).to redirect_to('/proposals/1') }
+  end
 
   context 'with real dependencies' do
+    let(:action) { described_class.new }
+    let(:proposal) { ProposalRepository.new.create(title: 'test', body: 'test') }
+    let(:params) { { proposal_id: proposal.id, name: 'test variant' } }
 
+    it { expect { subject }.to change { VariantRepository.new.all.count }.by(1) }
+    it { expect(subject).to redirect_to("/proposals/#{proposal.id}") }
   end
 end
