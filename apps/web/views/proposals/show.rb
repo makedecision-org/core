7c27e25da937bdfbded8e5765241c1e1b0f70268
Anton Davydov <antondavydov.o@gmail.com>
2018-08-20 19:55:13 +0300

    Create a form for adding new variants to the proposal

--- apps/web/views/proposals/show.rb
+++ apps/web/views/proposals/show.rb
@@ -1,5 +1,13 @@
 module Web::Views::Proposals
   class Show
     include Web::View
+
+    def add_variant_form
+      form_for :variant, routes.variants_path, { method: :post } do
+        input(name: 'proposal_id', type: 'hidden', value: proposal.id)
+        input(name: 'name', type: 'text', placeholder: 'New Variant', class: 'form-control')
+        submit 'Add', class: 'btn btn-primary'
+      end
+    end
   end
 end
