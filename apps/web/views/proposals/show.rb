0f3e874da161f5f0990a5c41bba419af0e0420f5
Anton Davydov <antondavydov.o@gmail.com>
2018-08-20 20:13:12 +0300

    Create form for negative traits

--- apps/web/views/proposals/show.rb
+++ apps/web/views/proposals/show.rb
@@ -18,5 +18,14 @@ module Web::Views::Proposals
         submit 'Add', class: 'btn btn-primary'
       end
     end
+
+    def add_negative_trait_form(variant)
+      form_for :trait, routes.traits_path, { method: :post, class: 'form-inline' } do
+        input(name: 'variant_id', type: 'hidden', value: variant.id)
+        input(name: 'trait_type', type: 'hidden', value: 'negative')
+        input(name: 'value', type: 'text', placeholder: 'Negative trait', class: 'form-control')
+        submit 'Add', class: 'btn btn-primary'
+      end
+    end
   end
 end
