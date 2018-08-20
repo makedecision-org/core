10a63e5ca511282815fada8cc2cabdb517952430
Anton Davydov <antondavydov.o@gmail.com>
2018-08-20 20:10:51 +0300

    Add specs for a new trait action

--- spec/web/controllers/traits/create_spec.rb
+++ spec/web/controllers/traits/create_spec.rb
@@ -1,9 +1,21 @@
 RSpec.describe Web::Controllers::Traits::Create, type: :action do
-  let(:action) { described_class.new }
-  let(:params) { Hash[] }
+  let(:action) { described_class.new(operation: operation) }
+  let(:params) { { variant_id: 1, value: 'new trait', trait_type: 'positive' } }
 
-  it 'is successful' do
-    response = action.call(params)
-    expect(response[0]).to eq 200
+  subject { action.call(params) }
+
+  context 'when operation returns success result' do
+    let(:operation) { -> (variant_id:, trait_type:, value:) { Success(Variant.new(proposal_id: 1)) } }
+
+    it { expect(subject).to redirect_to('/proposals/1') }
+  end
+
+  context 'with real dependencies' do
+    let(:action) { described_class.new }
+    let(:proposal) { ProposalRepository.new.create(title: 'test', body: 'test') }
+    let(:variant) { VariantRepository.new.create(name: 'variant #1', proposal_id: proposal.id) }
+    let(:params) { { variant_id: variant.id, trait_type: 'positive', value: 'new posititve trait' } }
+
+    it { expect(subject).to redirect_to("/proposals/#{proposal.id}") }
   end
 end
