f1a716fdb1a44ec62178969a8fc7b5d4a827b38d
Anton Davydov <antondavydov.o@gmail.com>
2018-08-20 20:11:04 +0300

    Create form for adding positive trait to variant

--- apps/web/views/proposals/show.rb
+++ apps/web/views/proposals/show.rb
@@ -9,5 +9,14 @@ module Web::Views::Proposals
         submit 'Add', class: 'btn btn-primary'
       end
     end
+
+    def add_positive_trait_form(variant)
+      form_for :trait, routes.traits_path, { method: :post, class: 'form-inline' } do
+        input(name: 'variant_id', type: 'hidden', value: variant.id)
+        input(name: 'trait_type', type: 'hidden', value: 'positive')
+        input(name: 'value', type: 'text', placeholder: 'Positive trait', class: 'form-control')
+        submit 'Add', class: 'btn btn-primary'
+      end
+    end
   end
 end
