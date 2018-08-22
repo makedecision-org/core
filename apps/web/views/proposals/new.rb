1d40aae7a29fe494fe53ce3102938ff08bc67b04
Anton Davydov <antondavydov.o@gmail.com>
2018-08-22 23:12:15 +0300

    Create a simpleform for adding proposals

--- apps/web/views/proposals/new.rb
+++ apps/web/views/proposals/new.rb
@@ -1,5 +1,21 @@
 module Web::Views::Proposals
   class New
     include Web::View
+
+    def form
+      form_for :proposal, '#', { method: :post } do
+        div(class: 'form-group') do
+          label :title
+          text_field :title, class: 'form-control', placeholder: 'Title'
+        end
+
+        div(class: 'form-group') do
+          label :body
+          text_area :body, class: 'form-control', placeholder: 'Description'
+        end
+
+        submit 'Suggest', class: 'btn btn-primary'
+      end
+    end
   end
 end
