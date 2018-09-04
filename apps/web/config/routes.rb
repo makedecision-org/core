f823308c0421f5da4d5a328829edd2c4cd292608
Anton Davydov <antondavydov.o@gmail.com>
2018-09-04 18:05:47 +0300

    Delete unnecessary proposal resources

--- apps/web/config/routes.rb
+++ apps/web/config/routes.rb
@@ -2,7 +2,9 @@ root to: 'home#index'
 
 get '/dashboard', to: 'organisations#index', as: :dashboard
 
-resources :proposals, only: %i[index new create show]
+resources :organisations, only: %i[show] do
+  resources :proposals, only: %i[new create show], controller: 'proposals'
+end
 
 # proposal routes
 resources :comments, only: %i[create]
@@ -10,7 +12,3 @@ resources :variants, only: %i[create destroy]
 resources :traits, only: %i[create]
 resources :proposal_statuses, only: %i[update]
 delete '/traits', to: 'traits#destroy', as: :trait
-
-resources :organisations, only: %i[show] do
-  resources :proposals, only: %i[new create show], controller: 'proposals'
-end
