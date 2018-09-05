90c510ba6db213b5be1e75b61eef1e50d4b9025b
Anton Davydov <antondavydov.o@gmail.com>
2018-09-05 12:54:44 +0300

    Fix slidebar

--- apps/web/views/organisations/show.rb
+++ apps/web/views/organisations/show.rb
@@ -11,8 +11,13 @@ module Web::Views::Organisations
 
     def sidebar_actions
       html do
-        li { link_to 'Add members', '#' }
-        li { link_to 'Create team', '#' }
+        li { link_to 'Add new', '#' }
+        div(class: 'dropdown-divider')
+
+        li { link_to 'Proposal', routes.new_organisation_proposal_path(organisation.slug) }
+        li { link_to 'Members', '#' }
+        li { link_to 'Team', '#' }
+        div(class: 'dropdown-divider')
       end
     end
   end
