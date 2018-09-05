8adbe549af5222aa46a94f15226a0ff0de3cf512
Anton Davydov <antondavydov.o@gmail.com>
2018-09-06 00:29:19 +0300

    Display list of team proposals on teams#show page

--- apps/web/controllers/teams/show.rb
+++ apps/web/controllers/teams/show.rb
@@ -2,14 +2,18 @@ module Web::Controllers::Teams
   class Show
     include Web::Action
     include Dry::Monads::Result::Mixin
-    include Import[operation: 'teams.operations.read']
+    include Import[
+      operation: 'teams.operations.read',
+      proposals_list_operation: 'teams.operations.proposals_list'
+    ]
 
-    expose :team
+    expose :team, :proposals
 
     def call(params)
       case result = operation.call(org_slug: params[:organisation_id], id: params[:id])
       when Success
         @team = result.value!
+        @proposals = proposals_list_operation.call(team_id: params[:id]).value!
       when Failure
         redirect_to routes.organisation_path(params[:organisation_id])
       end
--- spec/web/controllers/teams/show_spec.rb
+++ spec/web/controllers/teams/show_spec.rb
@@ -1,24 +1,30 @@
 RSpec.describe Web::Controllers::Teams::Show, type: :action do
-  let(:action) { described_class.new(operation: operation) }
+  let(:action) { described_class.new(operation: operation, proposals_list_operation: proposals_list_operation) }
   let(:params) { { organisation_id: 'test', id: 1 } }
 
   subject { action.call(params) }
 
   context 'when operation returns success result' do
     let(:operation) { -> (org_slug:, id:) { Success(Team.new) } }
+    let(:proposals_list_operation) { -> (team_id:) { Success('open' => [Proposal.new]) } }
 
     it { expect(subject).to be_success 200 }
 
     it 'returns list of proposals' do
       subject
       expect(action.team).to eq(Team.new)
+      expect(action.proposals).to eq('open' => [Proposal.new])
     end
   end
 
   context 'when operation returns failure result' do
     let(:operation) { -> (org_slug:, id:) { Failure(:not_found) } }
+    let(:proposals_list_operation) { -> (team_id:) { Success('open' => [Proposal.new]) } }
 
-    it { expect(subject).to redirect_to('/organisations/test') }
+    it do
+      expect(proposals_list_operation).to_not receive(:call)
+      expect(subject).to redirect_to('/organisations/test')
+    end
   end
 
   context 'whith a real dependency' do
