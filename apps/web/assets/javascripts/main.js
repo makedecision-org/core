7229cba6463a32fd59c2a11a28b72791dbd75d4a
Anton Davydov <antondavydov.o@gmail.com>
2018-09-05 12:46:41 +0300

    Turn on navbar for project

---
+++ apps/web/assets/javascripts/main.js
@@ -0,0 +1,9 @@
+  $(document).ready(function () {
+    $('.nav-item.nav-link').on('click', function () {
+      $('.nav-item.nav-link').removeClass('active');
+      $(this).addClass('active')
+
+      $('.nav-target').addClass('d-none');
+      $($(this).data('target')).removeClass('d-none')
+    });
+  });
