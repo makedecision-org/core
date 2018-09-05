1c9a0ace9a117484e913cd377ccd67573da6bb1e
Anton Davydov <antondavydov.o@gmail.com>
2018-09-05 23:35:03 +0300

    Get list of teams in proposal creating page

--- apps/web/controllers/proposals/new.rb
+++ apps/web/controllers/proposals/new.rb
@@ -1,11 +1,14 @@
 module Web::Controllers::Proposals
   class New
     include Web::Action
+    include Dry::Monads::Result::Mixin
+    include Import[team_list_operation: 'teams.operations.list']
 
-    expose :proposal
+    expose :proposal, :teams
 
     def call(params)
       @proposal = Proposal.new
+      @teams = team_list_operation.call(org_slug: params[:organisation_id]).value!
     end
   end
 end
--- spec/web/controllers/proposals/new_spec.rb
+++ spec/web/controllers/proposals/new_spec.rb
@@ -1,6 +1,24 @@
 RSpec.describe Web::Controllers::Proposals::New, type: :action do
-  let(:action) { described_class.new }
-  let(:params) { Hash[] }
+  let(:action) { described_class.new(team_list_operation: operation) }
+  let(:params) { { organisation_id: 'test' } }
 
-  it { expect(action.call(params)).to be_success }
+  subject { action.call(params) }
+
+  context 'when operation returns success result' do
+    let(:operation) { ->(org_slug:) { Success([Team.new]) } }
+
+    it { expect(subject).to be_success }
+
+    it do
+      action.call(params)
+      expect(action.proposal).to eq Proposal.new
+      expect(action.teams).to eq [Team.new]
+    end
+  end
+
+  context 'whith a real dependency' do
+    let(:action) { described_class.new }
+
+    it { expect(subject).to be_success 200 }
+  end
 end
