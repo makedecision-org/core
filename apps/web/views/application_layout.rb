c2aa290dfeb4890bdb1403426578e2e74712be52
Anton Davydov <antondavydov.o@gmail.com>
2018-09-08 17:43:38 +0300

    Use tooltip for open at field

--- apps/web/views/application_layout.rb
+++ apps/web/views/application_layout.rb
@@ -6,6 +6,12 @@ module Web
       include Web::Layout
 
       def sidebar_actions; end
+
+      def card_happened_at_tag(title, time)
+        html.p(class: 'card-text', title: time) do
+          "#{title} #{RelativeTime.in_words(time)}"
+        end
+      end
     end
   end
 end
