885598ee6877264ebaa95d3eb26018074847c1b7
Anton Davydov <antondavydov.o@gmail.com>
2018-09-17 22:18:51 +0300

    Implement action for creating polls

--- apps/web/controllers/polls/create.rb
+++ apps/web/controllers/polls/create.rb
@@ -4,8 +4,16 @@ module Web::Controllers::Polls
   class Create
     include Web::Action
 
-    def call(_params)
-      self.body = 'OK'
+    include Dry::Monads::Result::Mixin
+    include Import[operation: 'polls.operations.create']
+
+    def call(params)
+      case result = operation.call(**params.to_h)
+      when Success
+        redirect_to routes.organisation_proposal_path(result.value!.proposal_id)
+      when Failure
+        redirect_to routes.organisation_proposal_path(params[:proposal_id])
+      end
     end
   end
 end
